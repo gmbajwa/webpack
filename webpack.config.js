fe2b883e402019fb0cebff44737cf45fe6370218
Mustafa Ibraheemi <tekhbox@gmail.com>
2023-08-14 20:44:29 +0500

    htmlwebpackplugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin  = require('html-webpack-plugin');
 
 module.exports = {
 
@@ -15,5 +16,20 @@ module.exports = {
         path: path.resolve(__dirname, 'dis'),
         filename: '[name].[contenthash].js',
         clean: true
-    }
+    },
+
+    // webpack only understand js and json by default
+    // image file, css file, svg file does not know how to handle any type of file
+    // loaders do help the webpack to understand and scan css, html, imge or any other file tyle
+    // webpack(convert to modules and import (loaders))
+    // and more work is needed if then plugin or js function will help to loader to perform task
+
+    // loaders
+
+    //plugin
+    plugins: [new HtmlWebpackPlugin({
+        title: 'just a html page title',
+        name: 'index.html',
+        template: path.resolve(__dirname, 'src/template-for-index.html'),
+    })],
 }
